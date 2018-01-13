7b795e4dcf388e4f6e053cc865116361a7918233
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2018-01-13 17:54:48 +0530

    Reduced Space Usage by making all arrays of 4 into 1 word

--- Lab2/lab2_new.s
+++ Lab2/lab2_new.s
@@ -11,6 +11,10 @@ str r0,[r1] @ save the file handle
 
 ldr r0,=x
 ldr r1,=one
+
+mov r2,#1
+strb r2,[r1]
+
 bl copy_BCD
 
 @r10 is i
@@ -21,11 +25,11 @@ mov r9,#0
 
 	for_loop_outer:
 
-		ldr r0,=x
-		ldr r1,[r0]
-		ldr r2,[r0,#4]
-		ldr r3,[r0,#8]
-		ldr r4,[r0,#12]
+		@ldr r0,=x
+		@ldrb r1,[r0]
+		@ldrb r2,[r0,#1]
+		@ldrb r3,[r0,#2]
+		@ldrb r4,[r0,#3]
 
 		ldr r0,=y
 		ldr r1,=x
@@ -59,13 +63,13 @@ mov r9,#0
 
 			ldr r5,=dd
 			ldr r6,=y
-			ldr r7,[r6,r4]
+			ldrb r7,[r6,r4]
 			mul r6,r7,r7
-			str r6,[r5]
+			strb r6,[r5]
 
 			ldr r6,=dd
-			ldr r5,[r6,#0]
-			ldr r7,[r6,#4]
+			ldrb r5,[r6,#0]
+			ldrb r7,[r6,#1]
 				loop_start:
 
 
@@ -76,9 +80,9 @@ mov r9,#0
 
 
 				sub r5,r5,#10
-				str r5,[r6]
+				strb r5,[r6]
 				add r7,r7,#1
-				str r7,[r6,#4]
+				strb r7,[r6,#1]
 
 				b loop_start
 
@@ -89,8 +93,8 @@ mov r9,#0
 			ldr r2,=dd
 			bl add_BCD
 
-			add r4,r4,#4
-			cmp r4,#16
+			add r4,r4,#1
+			cmp r4,#4
 			blt loop_start2
 
 
@@ -103,7 +107,7 @@ mov r9,#0
 
 
 		ldr r0,=y
-		ldr r0,[r0,#0]
+		ldrb r0,[r0,#0]
 		b check_happy
 
 		return_check_happy:
@@ -131,13 +135,13 @@ mov r9,#0
         swi 0x6b
         ldr r1,=MatMsg1
         swi 0x69
-        ldr r1,[r7,#12]
+        ldrb r1,[r7,#3]
         swi 0x6b
-        ldr r1,[r7,#8]
+        ldrb r1,[r7,#2]
         swi 0x6b
-        ldr r1,[r7,#4]
+        ldrb r1,[r7,#1]
         swi 0x6b
-        ldr r1,[r7,#0]
+        ldrb r1,[r7,#0]
         swi 0x6b
         ldr r1,=MatMsg2
         swi 0x69
@@ -169,13 +173,13 @@ check_gt_1:
 @ r0 is *x
 	mov r1,r0
 	mov r0,#1
-	ldr r2,[r1,#4]
+	ldrb r2,[r1,#1]
 	cmp r2,#0
 	bgt ret
-	ldr r2,[r1,#8]
+	ldrb r2,[r1,#2]
 	cmp r2,#0
 	bgt ret
-	ldr r2,[r1,#12]
+	ldrb r2,[r1,#3]
 	cmp r2,#0
 	bgt ret
 	mov r0,#0
@@ -202,14 +206,14 @@ b return_check_happy
 @r0 is x[0]
 @r1 is s[0]
 copy_BCD:
-	ldr r3,[r1,#0]		@r3 is x0 
-	str r3,[r0,#0] 
-	ldr r3,[r1,#4]		@r3 is x1
-	str r3,[r0,#4]
-	ldr r3,[r1,#8]		@r3 is x2 
-	str r3,[r0,#8]
-	ldr r3,[r1,#12]	@r3 is x3 
-	str r3,[r0,#12]
+	ldrb r3,[r1,#0]		@r3 is x0 
+	strb r3,[r0,#0] 
+	ldrb r3,[r1,#1]		@r3 is x1
+	strb r3,[r0,#1]
+	ldrb r3,[r1,#2]		@r3 is x2 
+	strb r3,[r0,#2]
+	ldrb r3,[r1,#3]	@r3 is x3 
+	strb r3,[r0,#3]
  mov pc,lr
 
 
@@ -233,16 +237,16 @@ add_BCD:
 		ldr r1,=AA
 		mov r12,#0
 		ldr r2,[r1,#8]
-		ldr r2,[r2,r0]
+		ldrb r2,[r2,r0]
 		add r12,r12,r2
 		ldr r2,[r1,#4]
-		ldr r2,[r2,r0]
+		ldrb r2,[r2,r0]
 
 		add r12,r12,r2
 		add r12,r12,r3
 
 		ldr r1,[r1,#0]
-		str r12,[r1,r0]
+		strb r12,[r1,r0]
 
 		
 		mov r3,#0
@@ -251,14 +255,14 @@ add_BCD:
 		ble skip_if
 
 			sub r12,r12,#10
-			str r12,[r1,r0]
+			strb r12,[r1,r0]
 			mov r3,#1
 
 		skip_if:
 
 
-		add r0,r0,#4
-		cmp r0,#16
+		add r0,r0,#1
+		cmp r0,#4
 		blt loop
 
 	
@@ -281,14 +285,13 @@ add_BCD:
 
 
 	.data
-x: .word 0,0,0,0
-s: .word 0,0,0,0
-y: .word 0,0,0,0
-one: .word 1,0,0,0
-dd: .word 0,0,0,0
-zero: .word 0,0,0,0
-AA: .space 400
-BB: .space 400
+x: .word 0
+s: .word 0
+y: .word 0
+one: .word 0
+dd: .word 0
+zero: .word 0
+AA: .space 50
 MatMsg0: .asciz "number["
 MatMsg1: .asciz "] = "
 MatMsg2: .asciz "\n"
